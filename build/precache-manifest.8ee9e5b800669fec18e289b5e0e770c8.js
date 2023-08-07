f0ede89b7152aa2bf6884594ccbfd3ac23e0f8f2
mr_bhanu__33 <115440020+Bhanuprakashvasa@users.noreply.github.com>
2023-08-07 16:31:40 +0530

    Add files via upload

---
+++ build/precache-manifest.8ee9e5b800669fec18e289b5e0e770c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95db7946dccc653e69a1cb5a57b7b0ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63179694b5a8f1fafc66",
+    "url": "/static/css/main.3e443a13.chunk.css"
+  },
+  {
+    "revision": "3a08e988cb7a7a43f470",
+    "url": "/static/js/2.b1e358c0.chunk.js"
+  },
+  {
+    "revision": "38d5cc1cb1001b9cc38b06e2da080c18",
+    "url": "/static/js/2.b1e358c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63179694b5a8f1fafc66",
+    "url": "/static/js/main.91747c85.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.91747c85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36026393003bdfa4c6ad",
+    "url": "/static/js/runtime-main.711f2d1d.js"
+  }
+]);
